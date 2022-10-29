cd0e0381315dab9e1a1a197884945e3bcccfc33d
Mete <65303105+mectury@users.noreply.github.com>
2022-10-29 23:36:49 +0300

    added scc.c

---
+++ scc.c
@@ -0,0 +1,13 @@
+// Function declaration
+void myFunction();
+
+// The main method
+int main() {
+  myFunction();  // call the function
+  return 0;
+}
+
+// Function definition
+void myFunction() {
+  printf("I just got executed!");
+}
